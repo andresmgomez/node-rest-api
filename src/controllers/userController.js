2f0de9950bfe9c9f73a49ef5ff7fbf41a69cfb15
andresmgomez <andresmgomez23@gmail.com>
2023-05-17 09:41:43 -0500

    Fetch user's identity for authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,14 @@ const User = require("../models/userModel");
 
 exports.createUser = async (req, res) => {
   try {
-    let addUser = await User.create(req.body);
+    let addUser = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
     const newUser = addUser.save();
 
     if (newUser) {
@@ -18,6 +25,22 @@ exports.createUser = async (req, res) => {
     res.status(500).json({
       status: "error",
       message: "Invalid request! Unable to add user",
+      error: [err.message],
+    });
+  }
+};
+
+exports.findUserIndentities = async (req, res) => {
+  try {
+    const userIdentity = await User.find({}, { _id: 1 });
+
+    if (userIdentity) {
+      res.status(200).send(userIdentity);
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Invalid request! Unable to fetch users",
       error: [err.message],
     });
   }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/").post(userController.createUser);
+router.route("/findIdentities").get(userController.findUserIndentities);
 
 module.exports = router;
